ee7aee4e7fed0ccbf8a6bed1e0c99288a3674f86
ken_global <ken_global@qq.com>
2022-07-26 21:31:07 +0800

    2的第一次修改

--- shop_back/src/main/java/com/qf/shop/shop_back/controller/GoodsController.java
+++ shop_back/src/main/java/com/qf/shop/shop_back/controller/GoodsController.java
@@ -19,7 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-
+//一次
 @Controller
 @RequestMapping("/goods")
 public class GoodsController {
